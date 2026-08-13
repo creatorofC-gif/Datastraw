e528260c968640f01f4d69e7eb22b39dcd7229be
Naitik Gupta <naitikg1103@gmail.com>
2026-08-13 12:55:43 +0530

    Add comment to generateTicketId module
    Added a module comment to explain the ticket ID generation.

--- backend/src/utils/generateTicketId.js
+++ backend/src/utils/generateTicketId.js
@@ -1,3 +1,7 @@
+/**
+ * This module generates ticket ID by generating a random number preceded by a standard convention TKT 
+ */
+
 const generateTicketId = () =>{
     const randomNumber = Math.floor(100000 + Math.random() * 900000);
     return `TKT-${randomNumber}`;
