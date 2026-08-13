72c9141227e387d8ad90be264061854b43982785
Naitik Gupta <naitikg1103@gmail.com>
2026-08-13 12:58:05 +0530

    Added Comments

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -1,5 +1,13 @@
+/**
+ * This is the Schema definition of the notes section of the Ticket
+ * There are two namely 
+    1)ticket which will be grabbed as per the ID
+    2) noteText which will have the notes
+ */
+
+
 const mongoose = require("mongoose");
-const noteSchema = new mongoose.Schema(      //Note Database Schema
+const noteSchema = new mongoose.Schema(      
     {
         ticket: {
             type: mongoose.Schema.Types.ObjectId,
